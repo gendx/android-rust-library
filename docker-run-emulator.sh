e2ff7887c09ada0f1286e85abaa62b8ad2793cef
G. Endignoux <ggendx@gmail.com>
2022-11-08 22:26:13 +0100

    Remove unnecessary mounting of Xauthority files (which was incorrect).

--- docker-run-emulator.sh
+++ docker-run-emulator.sh
@@ -11,14 +11,12 @@ docker run \
     --memory-swappiness=0 \
     --env USER=dev \
     --env "DISPLAY=unix${DISPLAY}" \
-    --env XAUTHORITY=/Xauthority \
     --tmpfs /tmp:size=16m \
     --tmpfs /home/dev/build:exec,size=512m \
     --tmpfs /home/dev/.gradle:exec,size=512m \
     --tmpfs /home/dev/.android:size=3g \
     --tmpfs /home/dev/.cargo/registry:size=16m \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
-    --volume ${HOME}/.Xauthority:/Xauthority:ro \
     --device /dev/dri \
     --device /dev/kvm \
     android-rust-simd
